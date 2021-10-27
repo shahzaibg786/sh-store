1635573aec0801c44e23b36ed70a645515c15769
Shahzaib <44567763+shahzaibg786@users.noreply.github.com>
2021-10-27 09:38:47 -0700

    Adding Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello FROM hOME Page</h1>
+            <Navbar />
+
+        </div>
+    )
+}
+
+export default Home
