af49fefafb22c5198efd1298b85794f0013f4ef8
Shahzaib <44567763+shahzaibg786@users.noreply.github.com>
2021-10-28 03:47:48 -0700

    styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
         
         <Container>
             <Wrapper >
-            <Left >Left</Left>
+            <Left >
+                <Language>EN</Language>
+            </Left>
             <Center >Center</Center>
             <Right >Right</Right>
             </Wrapper>
@@ -26,8 +28,16 @@ height: 60px;
 const Wrapper = styled.div`
 padding: 10px 20px;
 display: flex;
-justify-content : space-between;
+justify-content: space-between;
 `
-const Left = styled.div``
-const Center = styled.div``
-const Right = styled.div``
+const Left = styled.div`
+flex:1;`
+
+const Language = styled.span`
+    font-size:14px;
+    cursor: pointer;
+`
+const Center = styled.div`
+flex: 1`
+const Right = styled.div`
+flex:1;`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from '../components/Navbar'
 const Home = () => {
     return (
         <div>
-            <h1>Hello FROM hOME Page</h1>
             <Navbar />
 
         </div>
